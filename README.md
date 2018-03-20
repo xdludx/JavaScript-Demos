19c698ff0be13a1b0b73dd37a13095161b430e53
dongxiao.lu <dongxiao.lu@qunar.com>
2018-03-20 18:02:46 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # JavaScript-Demos
 webpack + vue + element + sass
+
+### 开始
+
+`npm run dev`
+
+### build
+
+`npm run build`
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <div>
+      <el-button @click="startHacking">Starts</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It works!',
+        type: 'success',
+        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+        duration: 5000
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
